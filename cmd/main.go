3410025a5d5498e50f120b96265258a6cc3e3b25
Dmitry Popovich <dsp@mvdnet.org>
2025-06-25 10:14:35 +0800

    Start and shutdown refactor

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"net/http"
 	"os"
+	"os/signal"
+	"sync"
+	"time"
 
 	"github.com/Tualua/gozfsapi/internal/server"
 	"github.com/labstack/echo/v4"
@@ -11,7 +16,9 @@ import (
 	"go.uber.org/zap"
 )
 
-func main() {
+func run(ctx context.Context) error {
+	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
+	defer cancel()
 	swagger, err := server.GetSwagger()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error loading swagger spec\n: %s", err)
@@ -26,6 +33,8 @@ func main() {
 	defer logger.Sync()
 	zfsApi := server.NewApiServer(logger)
 	e := echo.New()
+	e.HideBanner = true
+	e.Use(middleware.Recover())
 	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
 		LogURI:    true,
 		LogStatus: true,
@@ -39,7 +48,34 @@ func main() {
 	}))
 	e.Use(oapimw.OapiRequestValidator(swagger))
 	server.RegisterHandlers(e, zfsApi)
-	logger.Fatal("starting server",
-		zap.Error(e.Start(":8080")),
-	)
+
+	go func() {
+		logger.Info("Starting server on :8080")
+		if err := e.Start(":8080"); err != nil && err != http.ErrServerClosed {
+			logger.Error("Server failed to start", zap.Error(err))
+		}
+	}()
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		<-ctx.Done()
+		shutdownCtx := context.Background()
+		shutdownCtx, cancel := context.WithTimeout(shutdownCtx, 10*time.Second)
+		defer cancel()
+		if err := e.Shutdown(shutdownCtx); err != nil {
+			fmt.Fprintf(os.Stderr, "error shutting down http server: %s\n", err)
+		}
+	}()
+	wg.Wait()
+	return nil
+}
+
+func main() {
+	ctx := context.Background()
+	if err := run(ctx); err != nil {
+		fmt.Fprintf(os.Stderr, "Error running server: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Server stopped gracefully")
 }
